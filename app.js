04612077f8889c6afc061fc866d5edc52c2dc330
Shubham <shubham.prasad21@gmail.com>
2021-09-10 13:58:58 +0530

    update server start message

--- app.js
+++ app.js
@@ -64,5 +64,5 @@ app.post("/success", (req, res) => {
 })
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server is running");
 })
